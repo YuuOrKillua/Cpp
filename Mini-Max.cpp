eb5460291c3042ef56cf5f2df581040b7dc3e5c7
Bruno <81570526+YuuOrKillua@users.noreply.github.com>
2024-04-08 18:42:03 -0300

    Update Mini-Max.cpp

--- Mini-Max.cpp
+++ Mini-Max.cpp
@@ -14,8 +14,8 @@ vector<string> split(const string &);
 
 void miniMaxSum(vector<int> arr) {
     int n = arr.size();
-    int min = 0;
-    int max = 0;
+    long long min = 0;
+    long long max = 0;
     
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n - i -1; j++){
