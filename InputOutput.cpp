5d44526d1c266f2178a75b2e786b838e6867b087
Bruno <81570526+YuuOrKillua@users.noreply.github.com>
2024-04-15 17:11:30 -0300

    Create InputOutput.cpp

---
+++ InputOutput.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    int n, n1, n2;
+    
+    cin >> n >> n1 >> n2;
+    
+    int soma = n + n1 + n2;
+    
+    cout << soma << endl;
+    
+    return 0;
+}
